2dd2f7860e1e5c2ab8823ed7de217e3d2968d2eb
Fraser <fbulbuc@ualberta.ca>
2020-02-22 16:42:42 -0700

    Intialized Google maps

--- client/app/src/main/java/com/invisibleink/explore/ExploreFragment.kt
+++ client/app/src/main/java/com/invisibleink/explore/ExploreFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.MapView
 import com.invisibleink.R
 import com.invisibleink.architecture.ViewProvider
 import com.invisibleink.extensions.findViewOrThrow
@@ -25,7 +26,7 @@ class ExploreFragment : Fragment(), ViewProvider {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
     override fun onAttach(context: Context) {
@@ -36,11 +37,51 @@ class ExploreFragment : Fragment(), ViewProvider {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewDelegate = ExploreViewDelegate(this)
+        viewDelegate.mapView?.onCreate(savedInstanceState)
         presenter.attach(viewDelegate)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         presenter.detach()
+    }
+
+    /**
+     * When using a [MapView] all life-cycle methods must be forwarded.
+     * See: https://developers.google.com/android/reference/com/google/android/gms/maps/MapView
+     */
+    override fun onStart() {
+        super.onStart()
+        viewDelegate.mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewDelegate.mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewDelegate.mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        viewDelegate.mapView?.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewDelegate.mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        viewDelegate.mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        viewDelegate.mapView?.onLowMemory()
     }
 }
--- client/app/src/main/java/com/invisibleink/explore/ExploreViewDelegate.kt
+++ client/app/src/main/java/com/invisibleink/explore/ExploreViewDelegate.kt
@@ -1,10 +1,30 @@
 package com.invisibleink.explore
 
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.invisibleink.R
 import com.invisibleink.architecture.BaseViewDelegate
 import com.invisibleink.architecture.ViewProvider
 
+
 class ExploreViewDelegate(viewProvider: ViewProvider) :
     BaseViewDelegate<ExploreViewState, ExploreViewEvent, ExploreDestination>(viewProvider) {
+
+    val mapView: MapView? = viewProvider.findViewById(R.id.exploreMapView)
+    private lateinit var map: GoogleMap
+
+    init {
+        mapView?.getMapAsync {
+            map = it
+            val quad = LatLng(53.527290, -113.527823)
+            map.addMarker(MarkerOptions().position(quad).title("Marker Title").snippet("Marker Description"))
+            map.moveCamera(CameraUpdateFactory.newLatLng(quad))
+        }
+    }
+
     override fun render(viewState: ExploreViewState): Unit? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
