0e571183e4220a0c0ec58ff1eb47c8130bbdf980
Fraser <fbulbuc@ualberta.ca>
2020-02-23 18:43:35 -0700

    Added basic settings layout

--- client/app/src/main/java/com/invisibleink/settings/SettingsFragment.kt
+++ client/app/src/main/java/com/invisibleink/settings/SettingsFragment.kt
@@ -1,17 +1,12 @@
 package com.invisibleink.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import com.invisibleink.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
 }
