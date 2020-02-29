b6b7c223bf35a6b5d9d4e234ebd67808ded4c10f
tamaramicevic <tamara.micevic@gmail.com>
2020-02-29 12:53:04 -0700

    Adding reporting and voting notes capability for database

--- server/src/azure-db/azure-cosmos-db.service.ts
+++ server/src/azure-db/azure-cosmos-db.service.ts
@@ -3,9 +3,9 @@ import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Point } from 'geojson';
 
+import { Vote } from 'src/shared/models/vote';
 import { NoteSearchParams } from 'src/shared/models/note-search-params';
 import { Note } from '../shared/models/note';
-import { NoteLifetimeLogSchema } from './models/note-lifetime-log-schema';
 import { NoteSchema } from './models/note-schema';
 import { ReportedNoteSchema } from './models/reported-note-schema';
 
@@ -17,7 +17,6 @@ export class AzureCosmosDbService implements OnApplicationBootstrap {
     private readonly mDBId: string;
     private readonly mNoteContainerId: string;
     private readonly mReportedNotesContainerId: string;
-    private readonly mNoteLifetimeLogContainerId: string;
     
     constructor(private readonly configService: ConfigService) {
         const endpoint: string = this.configService.get<string>('AZURE-COSMOS-DATABASE-END-POINT') || '<database services endpoint>';
@@ -25,29 +24,11 @@ export class AzureCosmosDbService implements OnApplicationBootstrap {
         this.mDBId = this.configService.get<string>('AZURE-COSMOS-DATABASE-ID') || '<database id>';
         this.mNoteContainerId = this.configService.get<string>('AZURE-COSMOS-NOTESCONTAINER-ID') || '<notes container id>';
         this.mReportedNotesContainerId = this.configService.get<string>('AZURE-COSMOS-REPORTSCONTAINER-ID') || '<reported notes container id>';
-        this.mNoteLifetimeLogContainerId = this.configService.get<string>('AZURE-COSMOS-LIFETIMECONTAINER-ID') || '<note lifetime log container id>';
-
+        
         this.mCosmosDbClient = new CosmosClient({
             endpoint,
             key: authKey,
           });
-
-        /* Note:    There should be no logic like this in constructors
-        // dummy values
-        const note: Note = {
-            NoteId: '22',
-            Title: 'TestNote',
-            Body: 'This is a test note',
-            TimeStamp: Date.now().toString(),
-            Score: 9,
-            Lat: 98.4569839,
-            Lon: 114.2364167,
-            ImageId: '22a',
-            ExpiresInHours: 24,
-        };
-
-        this.UploadNote(note);
-        */
     }
 
     async onApplicationBootstrap() {
@@ -62,7 +43,6 @@ export class AzureCosmosDbService implements OnApplicationBootstrap {
         // Create the containers if they don't exist
         try {
             await this.mCosmosDbClient.database(this.mDBId).containers.createIfNotExists({ id: this.mNoteContainerId });
-            await this.mCosmosDbClient.database(this.mDBId).containers.createIfNotExists({ id: this.mNoteLifetimeLogContainerId });
             await this.mCosmosDbClient.database(this.mDBId).containers.createIfNotExists({ id: this.mReportedNotesContainerId });
   
             const iterator = this.mCosmosDbClient.database(this.mDBId).containers.readAll();
@@ -71,14 +51,15 @@ export class AzureCosmosDbService implements OnApplicationBootstrap {
             // tslint:disable-next-line
             console.log('Error creating containers:\n', error);
         }
+
         console.log('Azure CosmosDb successfully initialized');
+
         return;
       }
 
     async UploadNote(note: Note): Promise<void> {
         try {
             const { database } = await this.mCosmosDbClient.databases.createIfNotExists({ id: this.mDBId });
-            const { container: noteLifetimeLogContainer } = await database.containers.createIfNotExists({ id: this.mNoteLifetimeLogContainerId });
             const { container: noteContainer } = await database.containers.createIfNotExists({ id: this.mNoteContainerId });
 
             // TODO: might need to verify coordinates are correctly entered
@@ -99,17 +80,6 @@ export class AzureCosmosDbService implements OnApplicationBootstrap {
             };
     
             const { item } = await noteContainer.items.create(dbNote);
-
-            // TODO: Evaluate if this needs to be removed
-            /*const dbNoteLifetimeLog: NoteLifetimeLogSchema = {
-                NoteId: item.id,
-                Timestamp: note.TimeStamp,
-                Lifetime: note.ExpiresInHours,
-            };
-
-            await noteLifetimeLogContainer.items.create(dbNoteLifetimeLog);
-
-            const { resources: results } = await noteLifetimeLogContainer.items.readAll().fetchAll();*/
 
         } catch (error) {
             // tslint:disable-next-line
@@ -161,15 +131,47 @@ export class AzureCosmosDbService implements OnApplicationBootstrap {
             // tslint:disable-next-line
             console.log('Error retrieving note:\n', error);
         }
-    }
 
-    async ReportNote(): Promise<void> {
-        // TODO : needs implementation
         return;
     }
 
-    async VoteNote(): Promise<void> {
-        // TODO : needs implementation
+    async ReportNote(report: ReportedNoteSchema): Promise<void> {
+        try {
+            const { database } = await this.mCosmosDbClient.databases.createIfNotExists({ id: this.mDBId });
+            const { container: reportedNotesContainer } = await database.containers.createIfNotExists({ id: this.mReportedNotesContainerId });
+
+            const dbReport: ReportedNoteSchema = {
+                NoteId: report.NoteId,
+                OptionalReport: report.OptionalReport
+            };
+    
+            const { item } = await reportedNotesContainer.items.create(dbReport);
+
+        } catch (error) {
+            // tslint:disable-next-line
+            console.log('Error reporting note:\n', error);
+        }
+
+        return;
+    }
+
+    async VoteNote(vote: Vote): Promise<void> {
+        try {
+            const { database } = await this.mCosmosDbClient.databases.createIfNotExists({ id: this.mDBId });
+            const { container: noteContainer } = await database.containers.createIfNotExists({ id: this.mNoteContainerId });
+
+            const item = noteContainer.item(vote.NoteId, undefined);
+            const { resource: note } = await item.read();
+
+            // depending on how we're passing from client
+            // either total score to update to, or [-1, +1] value to add
+            note.Score = vote.Score;
+
+            const { resource: updatedNote } = await noteContainer.items.upsert(note);
+
+        } catch (error) {
+            console.log('Error applying vote to note:\n', error);
+        }
         return;
     }
 }
--- server/src/azure-db/models/reported-note-schema.ts
+++ server/src/azure-db/models/reported-note-schema.ts
@@ -1,5 +1,4 @@
 export interface ReportedNoteSchema {
-    ReportId: string;
     NoteId: string;
     OptionalReport?: string;
 }
