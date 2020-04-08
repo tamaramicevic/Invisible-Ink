3593f45b77d1c82e23f289db1a10d33545b4a6d9
Adit Hasan <adit@ualberta.ca>
2020-04-08 12:41:56 -0600

    Added deployment script

---
+++ server/deployment-script.sh
@@ -0,0 +1,11 @@
+# Install npm and node
+apt-get update
+apt-get install nodejs
+apt-get install npm
+
+# Install dependencies
+npm install
+
+# Install pm2
+pm2 start npm --no-automation --name InvisibleInk -- run start
+
