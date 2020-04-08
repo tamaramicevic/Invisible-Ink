c3315239cd82309d40041020968d7da30491a16f
Adit Hasan <adit@ualberta.ca>
2020-04-08 12:53:06 -0600

    Added tests for retrieve notes

--- server/src/retrieve-notes/retrieve-notes.service.spec.ts
+++ server/src/retrieve-notes/retrieve-notes.service.spec.ts
@@ -1,5 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Note } from 'src/shared/models/note';
 import { RetrieveNotesService } from './retrieve-notes.service';
+import { Filter, PreBuiltFilter } from './models/retrieve-notes-request';
+
+const notes: Note[] = [
+  {
+    NoteId: 'noteA',
+    Title: 'noteA',
+    Body: 'noteA body',
+    Expiration: '234234',
+    Score: 5,
+    Lat: 25,
+    Lon: 58,
+    ImageId: 'image1.jpg',
+  },
+  {
+    NoteId: 'noteB',
+    Title: 'noteB',
+    Body: 'noteB body',
+    Expiration: '234234',
+    Score: 1,
+    Lat: 25,
+    Lon: 58,
+    ImageId: null,
+  },
+  {
+    NoteId: 'noteC',
+    Title: 'noteC',
+    Body: 'noteC body',
+    Expiration: '234234',
+    Score: 25,
+    Lat: 25,
+    Lon: 58,
+    ImageId: null,
+  },
+];
 
 describe('RetrieveNotesService', () => {
   let service: RetrieveNotesService;
@@ -14,5 +49,16 @@ describe('RetrieveNotesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  const filter: Filter = {
+    keywords: null,
+    options: PreBuiltFilter.BEST_RATED,
+    limit: 2,
+    withImage: null,
+  };
+
+  it('should be sorted by score', async () => {
+    expect(await service.ApplyFilters(null, notes, filter)).toBeDefined();
   });
 });
